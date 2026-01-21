d547bc158d7b291d490b525eea84b536fe2dbfbb
Sun Gang <31813857+sungang-cn@users.noreply.github.com>
2026-01-21 09:58:32 +0800

    Implement investment data analysis and visualization
    Load investment data, calculate total balance, and plot the results.

---
+++ invest.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load investment data from a CSV file
+#data = pd.read_csv('invest.csv', index_col='Date')
+data = pd.read_csv('invest.csv')
+data['Total_balance'] = data[['LQ', 'HF', 'JHL', 'GZ']].sum(axis=1)
+print(data['Date'])
+#print(data)
+data.to_excel('invest_output.xlsx')
+
+# 设置中文字体
+plt.rcParams['font.family'] = 'SimHei'
+plt.figure(figsize=(10, 6))
+plt.plot(data['index'], data['Total_balance'], marker='o', label='Total Balance', color='blue')
+plt.plot(data['index'], data['LQ'], marker='o', label='LQ', linestyle='--')
+plt.plot(data['index'], data['HF'], marker='o', label='HF', linestyle='--')
+plt.plot(data['index'], data['JHL'], marker='o', label='JHL', linestyle='--')
+plt.plot(data['index'], data['GZ'], marker='o', label='GZ', linestyle='--')
+# 设置非数字轴标签
+plt.xticks(data['index'], data['Date'], rotation=45)
+#plt.title('Investment Portfolio Over Weeks')
+plt.xlabel('周')
+plt.ylabel('收益')
+plt.legend()
+plt.grid()
+plt.show()
