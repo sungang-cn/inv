509eedbe5ee44666b09ca40d2c3c4b38cfa2cf76
Sun Gang <31813857+sungang-cn@users.noreply.github.com>
2026-02-25 09:25:02 +0800

    Round Total_balance and label plot points
    Round Total_balance to two decimal places and add labels to the Total_balance plot.

--- invest.py
+++ invest.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Load investment data from a CSV file
 #data = pd.read_csv('invest.csv', index_col='Date')
 data = pd.read_csv('invest.csv')
-data['Total_balance'] = data[['LQ', 'HF', 'JHL', 'GZ']].sum(axis=1)
+data['Total_balance'] = data[['LQ', 'HF', 'JHL', 'GZ']].sum(axis=1).round(2)
 #print(data['Date'])
 #print(data)
 data.to_excel('invest_output.xlsx')
@@ -20,6 +20,8 @@ plt.plot(data['index'], data['LQ'], marker='o', label='LQ', linestyle='--')
 plt.plot(data['index'], data['HF'], marker='o', label='HF', linestyle='--')
 plt.plot(data['index'], data['JHL'], marker='o', label='JHL', linestyle='--')
 plt.plot(data['index'], data['GZ'], marker='o', label='GZ', linestyle='--')
+for i in enumerate(data['Total_balance']):
+    plt.text(data['index'][i[0]] + 0.1, data['Total_balance'][i[0]] + 0.1, str(data['Total_balance'][i[0]]), fontsize=9)
 # 设置非数字轴标签
 plt.xticks(data['index'], data['Date'], rotation=45)
 #plt.title('Investment Portfolio Over Weeks')
